1d60544fc51363324304ef179043800a62d1ffa8
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2023-01-28 10:37:15 +0200

    Add entities

---
+++ DAL/Entities/News.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Entities
+{
+    public class News: EntityInfo
+    {
+        public string Id { get; set; } 
+        public DateTime? PublishingDate { get; set; }
+
+        public virtual Feed? Feed { get; set; }
+        public int FeedId { get; set; }
+    }
+}
